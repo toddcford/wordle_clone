6a88062be03d22e28d2f8849680707163ddf7ff7
toddcford <toddcford@gmail.com>
2022-02-27 20:18:46 -0600

    keyboard

--- Wordle.jsx
+++ Wordle.jsx
@@ -1,4 +1,5 @@
 
+import { useState, useEffect } from 'react'
 let BLACK       = '#666'
 let GREEN       = '#6aaa64'
 let GREY        = '#939598'
@@ -18,22 +19,74 @@ let wordList = [
   'crabs'
 ]
 
+let secret          = wordList[0]
+// let history         = ['piano', 'horse', 'toddf']
+// let currentAttempt  ='wat'
+
+
 export default function Wordle() {
+  let [history, setHistory] = useState([])
+  let [currentAttempt, setCurrentAttempt] = useState('')
+  let [isAnimating, setIsAnimating] = useState(false)
+
+  function handleKeyDown(e) {
+    if (e.ctrlKey || e.metaKey || e.altKey) {
+      return;
+    }
+    if (isAnimating) {
+      return;
+    }
+    handleKey(e.key.toLowerCase())
+  }
+
+  function handleKey(letter) {
+    if (letter === 'enter') {
+      if (currentAttempt.length < 5) {
+        console.log("premature enter");
+        return //Maybe wrong?
+      } 
+      if (!wordList.includes(currentAttempt)) {
+        alert('Not a word!');
+        return;
+      }
+      if (history.length === 5 && currentAttempt != secret) {
+        setHistory([...history, currentAttempt])
+        setTimeout(() => alert(secret.toUpperCase()), 2100);
+      }
+      if (currentAttempt === secret) {
+        setTimeout(() => alert("WINNER"), 2100);
+      }
+      setHistory([...history, currentAttempt])
+      setCurrentAttempt('')
+
+      // updateKeyboard()
+      //TODO: saveGame()
+      //TODO: pauseInput()
+      
+    } else if ( letter === 'backspace') {
+      setCurrentAttempt(currentAttempt.slice(0,-1))
+    } else if (/^[a-z]$/.test(letter)) {
+      if (currentAttempt.length < 5) {
+        setCurrentAttempt(currentAttempt + letter)
+        // animatePress(currentAttempt.length - 1)
+      }
+    }    
+  }
+ 
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown )
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  })  
   return (
     <div id="screen">
       <h1>Wordy</h1>
-      <Grid />
-      <Keyboard />
+      <Grid history={history} currentAttempt={currentAttempt} />
+      <Keyboard onKey={handleKey}/>
     </div>
   )
 }
 
-
-let secret          = wordList[0]
-let history         = ['piano', 'horse', 'toddf']
-let currentAttempt  ='wat'
-
-function Grid() {
+function Grid( { currentAttempt, history }) { 
   let rows = [];
   for (let i = 0; i < 6; i++) {
     if (i < history.length) {
@@ -106,13 +159,49 @@ function Cell({letter, attempt, index, solved}) {
   )
 }
 
-function Keyboard() {
+function Keyboard({ onKey }) {
   return (
     <div id="keyboard">
-      <h1>  </h1>
+      <KeyboardRow letters={"qwertyuiop"} onKey={onKey}  isLast={false}/>
+      <KeyboardRow letters={"asdfghjkl"} onKey={onKey} isLast={false}/>
+      <KeyboardRow letters={"zxcvbnm"} onKey={onKey} isLast={true}/>
     </div>
   );
 }
+
+function KeyboardRow( { letters, onKey, isLast }) {
+  let buttons = []
+  if (isLast) {
+    buttons.push(<Button key="enter" onKey={onKey} buttonKey="Enter"> Enter </Button>)
+  }
+  for (let letter of letters) {
+    buttons.push(<Button key={letter} onKey={onKey} buttonKey={letter}> { letter } </Button>)
+  }
+  if (isLast) {
+    buttons.push(<Button key="backspace" onKey={onKey} buttonKey="Backspace"> Back </Button>)
+  }
+  return (
+    <div className="keyboardRow">
+      {buttons}
+    </div>
+  )
+}
+
+function Button({ buttonKey, onKey, children }) {
+  return (
+    <button className="button"
+            id={buttonKey}
+            style={{
+              backgroundColor: LIGHTGREY
+            }}
+            onClick={() => {
+              onKey(buttonKey)
+            }}
+    > {children} </button>
+  )
+}
+
+
 
 function Attempt({attempt, solved}) {
   let cells =[]
