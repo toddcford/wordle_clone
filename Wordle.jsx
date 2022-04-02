82c59985a12aee784943ac37f74ab66e88f53341
toddcford <toddcford@gmail.com>
2022-04-02 13:15:56 -0500

    old changes

--- Wordle.jsx
+++ Wordle.jsx
@@ -1,6 +1,7 @@
 
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect, useRef } from 'react'
 import { KeyContext } from './context'
+
 let BLACK       = '#666'
 let GREEN       = '#6aaa64'
 let GREY        = '#939598'
@@ -25,19 +26,42 @@ let secret          = wordList[0]
 export default function Wordle() {
   let [history, setHistory] = useState([])
   let [currentAttempt, setCurrentAttempt] = useState('')
-  let [isAnimating, setIsAnimating] = useState(false)
+  let [bestColors, setBestColors] = useState(() => new Map())
+  // let [isAnimating, setIsAnimating] = useState(false)
+  let loadedRef = useRef(false)
+
+  // useEffect(() => {
+  //   if (loadedRef.current) {
+  //     console.log(currentAttempt)
+  //     return
+  //   }
+  //   loadedRef.current = true
+  //   let savedHistory = loadHistory()
+  //   console.log("saved history: ", savedHistory)
+  //   if (savedHistory) {
+  //     setHistory(savedHistory)
+  //   }
+  //   console.log("history state: ", history)
+  // })
+  
+  // useEffect(() => {
+  //   console.log("saving this history: ", history)
+  //   saveHistory(history)
+  // }, [history])
 
   function handleKeyDown(e) {
     if (e.ctrlKey || e.metaKey || e.altKey) {
       return;
     }
-    if (isAnimating) {
-      return;
-    }
+    // if (isAnimating) {
+    //   return;
+    // }
     handleKey(e.key.toLowerCase())
   }
-
+  
   function handleKey(letter) {
+    console.log("handling key")
+    letter = letter.toLowerCase();
     if (letter === 'enter') {
       if (currentAttempt.length < 5) {
         console.log("premature enter");
@@ -48,23 +72,33 @@ export default function Wordle() {
         return;
       }
       if (history.length === 5 && currentAttempt != secret) {
-        setHistory([...history, currentAttempt])
+        saveHistory(newHistory)
         setTimeout(() => alert(secret.toUpperCase()), 2100);
+        return
       }
       if (currentAttempt === secret) {
+        let newHistory = [...history, currentAttempt]
+        setBestColors(calculateBestColors(newHistory))
+        setHistory(newHistory)
+        setCurrentAttempt('')
         setTimeout(() => alert("WINNER"), 2100);
-      }
-      setHistory([...history, currentAttempt])
-      setCurrentAttempt('')
 
-      // updateKeyboard()
-      //TODO: saveGame()
+        return
+      }
+      let newHistory = [...history, currentAttempt]
+      setHistory(newHistory)
+      setCurrentAttempt('')
+      saveHistory(newHistory)
       //TODO: pauseInput()
+      setBestColors(calculateBestColors(newHistory))
       
     } else if ( letter === 'backspace') {
       setCurrentAttempt(currentAttempt.slice(0,-1))
     } else if (/^[a-z]$/.test(letter)) {
+      console.log("adding letter")
       if (currentAttempt.length < 5) {
+        console.log("setting current attempt")
+        console.log("history: ", history)
         setCurrentAttempt(currentAttempt + letter)
         // animatePress(currentAttempt.length - 1)
       }
@@ -80,7 +114,7 @@ export default function Wordle() {
       <KeyContext.Provider value={handleKey}>
         <h1>Wordy</h1>
         <Grid history={history} currentAttempt={currentAttempt} />
-        <Keyboard onKey={handleKey}/>
+        <Keyboard onKey={handleKey} bestColors={bestColors}/>
       </KeyContext.Provider>
     </div>
   )
@@ -159,23 +193,23 @@ function Cell({letter, attempt, index, solved}) {
   )
 }
 
-function Keyboard({ onKey }) {
+function Keyboard({ onKey, bestColors}) {
   return (
     <div id="keyboard">
-      <KeyboardRow letters={"qwertyuiop"}  isLast={false}/>
-      <KeyboardRow letters={"asdfghjkl"}  isLast={false}/>
-      <KeyboardRow letters={"zxcvbnm"}  isLast={true}/>
+      <KeyboardRow letters={"qwertyuiop"}  isLast={false} bestColors={bestColors} onKey={onKey}/>
+      <KeyboardRow letters={"asdfghjkl"}  isLast={false} bestColors={bestColors} onKey={onKey}/>
+      <KeyboardRow letters={"zxcvbnm"}  isLast={true} bestColors={bestColors} onKey={onKey}/>
     </div>
   );
 }
 
-function KeyboardRow( { letters, isLast }) {
+function KeyboardRow( { letters, isLast, onKey, bestColors }) {
   let buttons = []
   if (isLast) {
     buttons.push(<Button key="enter" buttonKey="Enter"> Enter </Button>)
   }
   for (let letter of letters) {
-    buttons.push(<Button key={letter} buttonKey={letter}> { letter } </Button>)
+    buttons.push(<Button key={letter} color={bestColors.get(letter)} buttonKey={letter}> { letter } </Button>)
   }
   if (isLast) {
     buttons.push(<Button key="backspace" buttonKey="Backspace"> Back </Button>)
@@ -187,13 +221,13 @@ function KeyboardRow( { letters, isLast }) {
   )
 }
 
-function Button({ buttonKey, children }) {
+function Button({ buttonKey, children, color=LIGHTGREY }) {
   const handleKey  = useContext(KeyContext)
   return (
     <button className="button"
             id={buttonKey}
             style={{
-              backgroundColor: LIGHTGREY
+              backgroundColor: color
             }}
             onClick={() => {
               handleKey(buttonKey)
@@ -201,8 +235,6 @@ function Button({ buttonKey, children }) {
     > {children} </button>
   )
 }
-
-
 
 function Attempt({attempt, solved}) {
   let cells =[]
@@ -233,4 +265,54 @@ function getBgColor(attempt, i) {
      return GREEN;
    }
    return YELLOW;
+}
+
+function loadHistory() {
+  let data
+  try {
+    data = JSON.parse(localStorage.getItem('data'));
+  } catch {}
+  if (data !== null) {
+    if (data.secret === secret) {
+      console.log("returning history")
+      return Object.values(data.history)
+    }
+  }
+}
+function saveHistory(history) {
+  let data = JSON.stringify({
+    secret,
+    history
+  })
+  try {
+    localStorage.setItem('data', data) 
+    console.log("saving historys")
+  } catch { 
+    console.log("failed to save history")
+  }
+}
+
+function calculateBestColors(history) {
+  let map = new Map()
+  for (let attempt of history) { 
+    for (let i = 0; i < attempt.length; i++) {
+      let color = getBgColor(attempt, i)
+      let key   = attempt[i]
+      let bestColor = map.get(key)
+      map.set(key, getBetterColor(color, bestColor))
+    }
+  }
+  
+  return map
+}
+
+function getBetterColor(a, b) {
+  if (a === GREEN || b === GREEN) {
+    return GREEN
+  }
+  if (a === YELLOW || b === YELLOW) {
+    return YELLOW
+  }
+
+  return GREY
 }
